fc1b80684b3c37683b1d4a670b43836680709d49
akyao <konama.akyao@gmail.com>
2016-04-25 22:15:59 +0900

    表示するとちゅう

--- js/hoge.js
+++ js/hoge.js
@@ -2,6 +2,10 @@ const electron = require('electron');
 const app = electron.app;
 const fs = require('fs');
 
+// console.log($);
+// const jq = require('jQuery');
+window.jQuery = window.$ = require('./bower_components/jquery/dist/jquery.min.js');
+
 class AnsFile {
   constructor(filePath) {
     this._filePath = filePath;
@@ -42,5 +46,18 @@ electron.ipcRenderer.on('ping', function(event, message) {
   var targetDir = 'benchmarks/wordpress-nginx';
   var fileList = load(targetDir);
 
-  console.log(fileList);
+  // 表示を初期化
+  $(".tbody tr:visible").remove();
+
+  // templateを取得
+  var template = $(".trTemplate");
+  var tbody = $(".tbody");
+
+  // TODO 表示する
+  for (var i = 0; i < fileList.length; i++) {
+    var ansFile = fileList[i];
+    var newTr = template.clone(true);
+    newTr.appendTo(tbody);
+    newTr.show();
+  }
 });
