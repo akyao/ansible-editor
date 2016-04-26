5f214c72cbf9168089d0a1b4ca7426f8b0dbbd6d
akyao <konama.akyao@gmail.com>
2016-04-26 09:03:10 +0900

    fileType実装中

--- js/hoge.js
+++ js/hoge.js
@@ -2,16 +2,15 @@ const electron = require('electron');
 const app = electron.app;
 const fs = require('fs');
 
-// console.log($);
-// const jq = require('jQuery');
 window.jQuery = window.$ = require('./bower_components/jquery/dist/jquery.min.js');
 
 class AnsFile {
-  constructor(dirPath, fileName, depth) {
+  constructor(dirPath, fileName, depth, parent) {
     this._dirPath = dirPath;
     this._fileName = fileName
     this._children = [];
     this._depth = depth;
+    this._parent = parent;
   }
   get filePath() {
     return this._dirPath + "/" + this._fileName;
@@ -24,6 +23,21 @@ class AnsFile {
   }
   get depth() {
     return this._depth;
+  }
+  fileType() {
+    var fileType = null;
+    if (this._parent != null) {
+      fileType = this._parent.fileType();
+    }
+    if (fileType == null) {
+      if ( "roles" == this._fileName) {
+        return "role";
+      }
+      "host", "yml"
+    } else {
+      return "other";
+    }
+    return fileType;
   }
   add(child) {
     if (Array.isArray(child)) {
@@ -41,14 +55,14 @@ class AnsFile {
   }
 }
 
-function load(dirPath, depth) {
+function load(dirPath, parent, depth) {
   var pathList = fs.readdirSync(dirPath);
   var ansFileList = [];
   for (var i = 0; i < pathList.length; i++) {
     var fileName = pathList[i];
-    var ansFile = new AnsFile(dirPath, fileName, depth);
+    var ansFile = new AnsFile(dirPath, fileName, depth, parent);
     if (ansFile.isDir()) {
-      ansFile.add(load(ansFile.filePath, depth + 1));
+      ansFile.add(load(ansFile.filePath, ansFile, depth + 1));
     }
     ansFileList.push(ansFile);
   }
@@ -58,8 +72,11 @@ function load(dirPath, depth) {
 electron.ipcRenderer.on('ping', function(event, message) {
 
   var targetDir = 'benchmarks/wordpress-nginx';
-  var fileList = load(targetDir, 0);
+  var fileList = load(targetDir, null, 0);
 
+  // TODO sort
+
+  // サブフォルダの中ではmain.ymlが先頭
   // 表示を初期化
   $(".tbody tr:visible").remove();
 
@@ -80,10 +97,9 @@ electron.ipcRenderer.on('ping', function(event, message) {
       }
       newTr.appendTo(tbody);
 
-      $(".fileName", newTr).append(ansFile.fileName)
+      $(".fileName", newTr).append(ansFile.fileName + ":" + ansFile.fileType());
       newTr.show();
       if (ansFile.hasChild()) {
-        // 再帰
         render(ansFile.children);
       }
     }
